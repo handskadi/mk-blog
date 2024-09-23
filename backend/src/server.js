b1ce5ca0bf0c26147a11847781a757c86bb1e3b4
Mohamed KADI <handskadi@gmail.com>
2024-09-23 06:01:39 +0200

    Dababase setup, replace memory data with actual DB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,49 +1,70 @@
 import express from "express";
+import { conn_db, db } from "./db.js";
 
 const app = express();
-
-let articlesInfo = [
-  {
-    name: "learn-react",
-    upvotes: 0,
-    comments: [],
-  },
-  {
-    name: "learn-node",
-    upvotes: 0,
-    comments: [],
-  },
-  {
-    name: "mongodb",
-    upvotes: 0,
-    comments: [],
-  },
-];
 app.use(express.json());
 
-app.put("/api/articles/:name/upvote", (req, res) => {
+//
+app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
-  const article = articlesInfo.find((a) => a.name === name);
+
+  const article = await db.collection("articles").findOne({ name });
   if (article) {
-    article.upvotes += 1;
+    res.json(article);
+  } else {
+    res.status(404).send("Article not found");
+  }
+});
+
+// Upvote API
+app.put("/api/articles/:name/upvote", async (req, res) => {
+  const { name } = req.params;
+
+  await db.collection("articles").updateOne(
+    { name },
+    {
+      $inc: { upvotes: 1 },
+    }
+  );
+
+  const article = await db.collection("articles").findOne({ name });
+
+  if (article) {
     res.send(`The ${name} article now has ${article.upvotes} upvotes.`);
   } else {
     res.send(`The artilce Not Exsists`);
   }
 });
 
-app.post("/api/articles/:name/comments", (req, res) => {
+// Comments API
+app.post("/api/articles/:name/comments", async (req, res) => {
   const { name } = req.params;
-  const article = articlesInfo.find((a) => a.name === name);
   const { postedBy, comment } = req.body;
+
+  await db.collection("articles").updateOne(
+    { name },
+    {
+      $push: {
+        comments: {
+          postedBy,
+          comment,
+        },
+      },
+    }
+  );
+
+  const article = await db.collection("articles").findOne({ name });
+
   if (article) {
-    article.comments.push({ postedBy, comment });
     res.send(article.comments);
   } else {
     res.send(`The artilce Not Exsists`);
   }
 });
 
-app.listen(8000, () => {
-  console.log("Server is listening on port 8000");
+conn_db(() => {
+  console.log("Successfully connected to database!");
+  app.listen(8000, () => {
+    console.log("Server is listening on port 8000");
+  });
 });
