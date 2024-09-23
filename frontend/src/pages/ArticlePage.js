9b46b3d065bb712795778fa8934734d21df29fd4
Mohamed KADI <handskadi@gmail.com>
2024-09-23 02:40:24 +0200

    Backend init: install express and setup for server build

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,14 +1,19 @@
 import { useParams } from "react-router-dom";
 import articles from "../data/article-content";
+import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
   const { articleId } = useParams();
   const article = articles.find((article) => article.name == articleId);
+
+  if (!article) {
+    return <NotFoundPage />;
+  }
   return (
     <>
       <h1>{article.title}</h1>
-      {article.content.map((pargraph) => (
-        <p>{pargraph}</p>
+      {article.content.map((pargraph, i) => (
+        <p key={i}>{pargraph}</p>
       ))}
     </>
   );
--- frontend/src/pages/ArticlesListPage.js
+++ frontend/src/pages/ArticlesListPage.js
@@ -1,16 +1,11 @@
+import ArticlesList from "../Components/ArticlesList";
 import articles from "../data/article-content";
-import { Link } from "react-router-dom";
 
 const ArticlesListPage = () => {
   return (
     <>
       <h1>Articles</h1>
-      {articles.map((articles) => (
-        <Link to={`/articles/${articles.name}/`}>
-          <h3>{articles.title}</h3>
-          <p>{articles.content[0].substring(0, 150)}...</p>
-        </Link>
-      ))}
+      <ArticlesList articles={articles} />
     </>
   );
 };
